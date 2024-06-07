3d36bbabb781d432841abcb2c697f4bd8a65bb48
zhorik06bondarenko <zhorik06bondarenko@gmail.com>
2024-06-08 00:04:12 +0500

    connected db

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="{inter.className}">
+        <body className="{inter.className} h-screen">
           <ToastProvider />
           {children}
         </body>
