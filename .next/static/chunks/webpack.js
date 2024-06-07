f92b1c3da31b05056cf7d1304b6c77801789da04
zhorik06bondarenko <zhorik06bondarenko@gmail.com>
2024-06-07 15:28:56 +0500

    git add .

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89a26418dba60938"; }
+/******/ 		__webpack_require__.h = function() { return "f880a5d4eb802d36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
