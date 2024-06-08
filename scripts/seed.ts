529e281a84f76cb2bc3b02e04514534de3624c17
zhorik06bondarenko <zhorik06bondarenko@gmail.com>
2024-06-09 00:44:08 +0500

    seed.ts

---
+++ scripts/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+        data: [
+            { name: "Computer Science" },
+            { name: "CSS" },
+            { name: "Mongo DB" },
+            { name: "HTML" },
+            { name: "MySQL" },
+            { name: "PostgreSQL" },
+        ]
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seedint the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main()
