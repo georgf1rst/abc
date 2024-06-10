2771bcee9dfd61e27c113c34c7739657a8ce5f54
zhorik06bondarenko <zhorik06bondarenko@gmail.com>
2024-06-10 11:45:35 +0500

    reorder-chapters-functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -15,15 +15,15 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
-import { Course } from "@prisma/client";
+import { Chapter, Course } from "@prisma/client";
+import { ChaptersList } from "./chapters-list";
 
 interface ChaptersFormProps {
-  initialData: Course
+  initialData: Course & { chapters: Chapter[] };
   courseId: string;
 };
 
@@ -64,8 +64,32 @@ export const ChaptersForm = ({
     }
   }
 
-  return(
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData
+      })
+      toast.success("Chapters reordered");
+      router.refresh
+    } catch {
+      toast.error("Something went wrong")
+    } finally {
+      setIsUpdating(false);
+    }
+  }
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+  }
+
+  return (
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700"/>
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course chapters 
         <Button onClick={toggleCreating} variant="ghost">
@@ -79,14 +103,6 @@ export const ChaptersForm = ({
           )}
         </Button>
       </div>
-      {!isCreating && (
-        <p className={cn(
-          "text-sm mt-2",
-          !initialData.description && "text-slate-400 italic"
-        )}>
-          {initialData.description || "No description"}
-        </p>
-      )}
       {isCreating && (
         <Form {...form}>
           <form
@@ -99,9 +115,9 @@ export const ChaptersForm = ({
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Textarea 
+                    <Input 
                       disabled={isSubmitting}
-                      placeholder="e.g. 'This course is about...'"
+                      placeholder="e.g. 'Introduction to the course'"
                       {...field}
                     />
                   </FormControl>
@@ -109,16 +125,32 @@ export const ChaptersForm = ({
                 </FormItem>
               )}
             />
-            <div className="flex items-center gap-x-2">
               <Button
                 disabled={!isValid || isSubmitting}
                 type="submit"
                 >
-                Save
+                Create
               </Button>
-            </div>
           </form>
         </Form>
+      )}
+      {!isCreating && (
+        <div className={cn(
+          "text-sm mt-2",
+          !initialData.chapters.length && "text-slate-500 italic"
+        )}>
+          {!initialData.chapters.length && "No chapters"}
+          <ChaptersList 
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
+        </div>
+      )}
+      {!isCreating && (
+        <p className="text-xs text-muted-foreground mt-4">
+          Drag and drop to reorder the chapters
+        </p>
       )}
     </div>
   )
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -13,7 +13,7 @@ const handleAuth = () => {
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+  courseImage: f({ image: { maxFileSize: "8MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
   courseAttachment: f(["text", "image", "video", "audio", "pdf"])
