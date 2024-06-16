0dbcc68d28c6fb34bc10506ef35fcab9ba28dd57
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-16 17:03:08 +0500

    fix-request

--- app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
-    req: RequestMode,
+    req: Request,
     { params }: { params: { courseId: string; chapterId: string} }
 ){
     try {
